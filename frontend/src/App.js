641cdcff4f8ad696e1756264f15e336bfd037bbb
Yaroslav Pashynskyi <yaroslav.pashynskyi@gmail.com>
2023-06-21 14:18:19 +0300

    Display and delete images

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,6 +2,8 @@ import 'bootswatch/dist/minty/bootstrap.min.css';
 import Header from './components/Header';
 import Search from './components/Search';
 import { useState } from 'react';
+import ImageCard from './components/ImageCard';
+import { Container, Row, Col } from 'react-bootstrap';
 
 const UNSPLASH_KEY = process.env.REACT_APP_UNSPLASH_API;
 
@@ -9,21 +11,22 @@ function App() {
   const [query, setQuery] = useState('');
   const [images, setImages] = useState([]);
 
-  console.log(images);
-
   const handleSearchSubmit = (e) => {
     e.preventDefault();
-    console.log(query);
     fetch(
       `https://api.unsplash.com/photos/random/?query=${query}&client_id=${UNSPLASH_KEY}`
     )
       .then((res) => res.json())
       .then((data) => {
-        setImages([data, ...images]);
+        setImages([{ ...data, title: query }, ...images]);
       })
       .catch((error) => console.log(error));
 
     setQuery('');
+  };
+
+  const handleDeleteImage = (id) => {
+    setImages(images.filter((images) => images.id !== id));
   };
 
   return (
@@ -34,6 +37,15 @@ function App() {
         setQuery={setQuery}
         handleSubmit={handleSearchSubmit}
       />
+      <Container className="mt-4">
+        <Row xs={1} md={2} lg={3}>
+          {images.map((image, i) => (
+            <Col className="pb-3" key={i}>
+              <ImageCard image={image} deleteImage={handleDeleteImage} />
+            </Col>
+          ))}
+        </Row>
+      </Container>
     </div>
   );
 }
