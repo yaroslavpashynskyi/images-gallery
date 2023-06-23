30b8b49302293952a1c1159d4a41a2ae1b5d8e01
Yaroslav Pashynskyi <yaroslav.pashynskyi@gmail.com>
2023-06-23 12:05:52 +0300

    Change api

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -6,7 +6,7 @@ import ImageCard from './components/ImageCard';
 import { Container, Row, Col } from 'react-bootstrap';
 import Welcome from './components/Welcome';
 
-const UNSPLASH_KEY = process.env.REACT_APP_UNSPLASH_API;
+const API_URL = process.env.REACT_APP_API_URL || 'http://127.0.0.1:5050';
 
 function App() {
   const [query, setQuery] = useState('');
@@ -14,9 +14,7 @@ function App() {
 
   const handleSearchSubmit = (e) => {
     e.preventDefault();
-    fetch(
-      `https://api.unsplash.com/photos/random/?query=${query}&client_id=${UNSPLASH_KEY}`
-    )
+    fetch(`${API_URL}/new-image?query=${query}`)
       .then((res) => res.json())
       .then((data) => {
         setImages([{ ...data, title: query }, ...images]);
